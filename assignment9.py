c625cc9219e4440e565dc90ada32479ce61ff3a4
mehmetkoymen <koymen.m@yandex.com>
2022-10-30 16:29:04 +0300

    asiignment 9

---
+++ assignment9.py
@@ -0,0 +1,8 @@
+password = {"mehmet": "sadf1234"}
+
+name = input("enter your name:")
+
+if name == "mehmet" :
+    print(f"Hello Mehmet! The password is: {password.values()} ")
+else:
+    print(f"hello {name}! See you later")
